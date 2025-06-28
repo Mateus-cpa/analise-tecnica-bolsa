19a571af04f7763fc745c4034ebc0fbda6d69984
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-06-28 08:13:16 -0300

    :importa tickers de índices b3

--- src/importar_tickers.py
+++ src/importar_tickers.py
@@ -9,6 +9,9 @@ def importar_tickers():
     if not os.path.exists('raw_data'):
         os.makedirs('raw_data')
 
+    tickers = []
+
+    # -- AÇÕES --
     url = "https://www.dadosdemercado.com.br/acoes"
     request = requests.get(url)
     content = request.content.decode('utf-8')
@@ -19,12 +22,29 @@ def importar_tickers():
     if table is None:
         raise ValueError("Tabela não encontrada no conteúdo HTML.")
     # Extraindo os tickers do conteúdo HTML
-    tickers = []
     for row in table.find_all('tr')[1:]:  # Ignora o cabeçalho
         cols = row.find_all('td')
         if len(cols) > 0:
             ticker = cols[0].text.strip()
             tickers.append(ticker)
+    
+    # -- ÍNDICES B3 --
+    url = "https://www.dadosdemercado.com.br/b3"
+    request = requests.get(url)
+    content = request.content.decode('utf-8')
+    #ler html, pegando a table para achar os códigos da b3 com bs4:
+    soup = bs4(content, 'html.parser')
+    table_container = soup.find('div', {'class': 'table-container'})
+    table = table_container.find('table') if table_container else None
+    if table is None:
+        raise ValueError("Tabela não encontrada no conteúdo HTML.")
+    # Extraindo os tickers do conteúdo HTML
+    for row in table.find_all('tr')[1:]:  # Ignora o cabeçalho
+        cols = row.find_all('td')
+        if len(cols) > 0:
+            ticker = cols[0].text.strip()
+            tickers.append(ticker)
+
     tickers = list(set(tickers))  # Remove duplicatas
     # Salvando os tickers em um arquivo CSV
     with open('raw_data/tickers.csv', 'w', encoding='utf-8') as file:
