dd0ae5fb1250b054fe725af040a4941798410007
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-07-09 06:49:14 -0300

    :sparkles: feat: coloca previsão dos analistas como opcional no gráfico

--- src/plotar_grafico.py
+++ src/plotar_grafico.py
@@ -173,38 +173,39 @@ def plotar_grafico(acao, ticker):
             name='Início previsão',
             marker=dict(color='blue', size=20, symbol='hourglass')
         ))
-    
+        
     # Adiciona ponto diamante para targetMedianPrice na última linha, se existir
-    if 'targetMedianPrice' in acao.columns and pd.notnull(acao['targetMedianPrice'].iloc[-1]):
-        fig.add_trace(go.Scatter(
-            x=[acao.index[-1]],
-            y=[acao['targetMedianPrice'].iloc[-1]],
-            mode='markers',
-            name='Previsão dos analistas',
-            marker=dict(color='purple', size=10, symbol='diamond')
-        ))
-    
-        # Adiciona seta do último fechamento para o targetMedianPrice
-        # Encontra o último índice com Close não nulo
-        ultimo_close_idx = acao[acao['Close'].notnull()].index[-1]
-        ultimo_close_val = acao.loc[ultimo_close_idx, 'Close']
+    if col5.checkbox('Previsão dos analistas',value=True):
+        if 'targetMedianPrice' in acao.columns and pd.notnull(acao['targetMedianPrice'].iloc[-1]):
+            fig.add_trace(go.Scatter(
+                x=[acao.index[-1]],
+                y=[acao['targetMedianPrice'].iloc[-1]],
+                mode='markers',
+                name='Previsão dos analistas',
+                marker=dict(color='purple', size=10, symbol='diamond')
+            ))
+        
+            # Adiciona seta do último fechamento para o targetMedianPrice
+            # Encontra o último índice com Close não nulo
+            ultimo_close_idx = acao[acao['Close'].notnull()].index[-1]
+            ultimo_close_val = acao.loc[ultimo_close_idx, 'Close']
 
-        # Adiciona seta do último fechamento real para o targetMedianPrice
-        fig.add_annotation(
-            x=acao.index[-1],
-            y=acao['targetMedianPrice'].iloc[-1],
-            ax=ultimo_close_idx,
-            ay=ultimo_close_val,
-            xref="x", yref="y",
-            axref="x", ayref="y",
-            showarrow=True,
-            arrowhead=3,
-            arrowsize=1.5,
-            arrowwidth=2,
-            arrowcolor="purple",
-            opacity=0.8,
-            text="",
-        )
+            # Adiciona seta do último fechamento real para o targetMedianPrice
+            fig.add_annotation(
+                x=acao.index[-1],
+                y=acao['targetMedianPrice'].iloc[-1],
+                ax=ultimo_close_idx,
+                ay=ultimo_close_val,
+                xref="x", yref="y",
+                axref="x", ayref="y",
+                showarrow=True,
+                arrowhead=3,
+                arrowsize=1.5,
+                arrowwidth=2,
+                arrowcolor="purple",
+                opacity=0.8,
+                text="",
+            )
     
     fig.update_layout(title=f"Gráfico de Preços - {ticker.split('.')[0]}", xaxis_title='Data', yaxis_title='Preço (R$)')
  
