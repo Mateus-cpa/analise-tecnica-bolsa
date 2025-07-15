b768f0483f355646f7274c58c6ed0bdab43c3e7a
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-07-15 07:33:46 -0300

    :broom: feat: alterta posição das colunas de fundamentos para melhor distribuição

--- src/mostrar_fundamentos.py
+++ src/mostrar_fundamentos.py
@@ -26,10 +26,9 @@ def mostrar_fundamentos(fundamentos: pd.DataFrame):
     if fundamentos.empty:
         st.error("Nenhum dado fundamental disponível para o ticker selecionado.")
         return
-    # Exibe os fundamentos no Streamlit
+    # Exibe kpis dos fundamentos no Streamlit
     col1, col2, col3 = st.columns(3)
     with col1:
-        #kpis
         if fundamentos['previousClose'].values[0] is not None:
             variacao = fundamentos['regularMarketChangePercent'].values[0]
             st.metric(
@@ -38,11 +37,37 @@ def mostrar_fundamentos(fundamentos: pd.DataFrame):
                 delta=f"{variacao:.2f}%",
                 delta_color="normal"
             )
+
+        if fundamentos['profitMargins'].values[0] is not None:
+            st.metric("Margem de Lucro", f"{fundamentos['profitMargins'].values[0]:.2%}")
+        
+        if fundamentos['numberOfAnalystOpinions'].values[0] is not None:
+            st.metric('Nº de Opiniões de Analistas', fundamentos['numberOfAnalystOpinions'].values[0])
+
+    with col2:
         dy = fundamentos['dividendYield'].values[0]
         if pd.notnull(dy) and dy != 'N/A':
             st.metric("Dividend Yield", f"{float(dy)/100:.2%}")
+
+        
+        if fundamentos['recommendationKey'].values[0] is not None:
+            if 'buy' in fundamentos['recommendationKey'].values[0]:
+                st.metric("Recomendação", "Comprar", delta_color="normal")
+                #st.metric("Recomendação", ":cow: [Comprar]", delta_color="normal")
+            elif 'sell' in fundamentos['recommendationKey'].values[0]:
+                st.metric("Recomendação", "Vender", delta_color="inverse")
+                #st.metric("Recomendação", ":bear: [Vender]", delta_color="inverse")
+        
+        if fundamentos['targetMeanPrice'].values[0] is not None:
+            st.metric('Média de Preço alvo', f"R$ {fundamentos['targetMeanPrice'].values[0]:.2f}")
+
+        if fundamentos['targetMedianPrice'].values[0] is not None:
+            st.metric('Mediana de Preço alvo', f"R$ {fundamentos['targetMedianPrice'].values[0]:.2f}")
+
+    with col3:
         if fundamentos['lastDividendValue'].values[0] is not None:
             st.metric("Último Dividendo", f"R$ {fundamentos['lastDividendValue'].values[0]:.2f}")
+        
         last_div_date = fundamentos['lastDividendDate'].values[0]
         if pd.notnull(last_div_date):
             try:
@@ -53,28 +78,12 @@ def mostrar_fundamentos(fundamentos: pd.DataFrame):
         else:
             st.metric("Data do Último Dividendo", "N/A")
 
-    with col2:
-        if fundamentos['profitMargins'].values[0] is not None:
-            st.metric("Margem de Lucro", f"{fundamentos['profitMargins'].values[0]:.2%}")
-        if fundamentos['recommendationKey'].values[0] is not None:
-            if 'buy' in fundamentos['recommendationKey'].values[0]:
-                st.metric("Recomendação", "Comprar", delta_color="normal")
-                #st.metric("Recomendação", ":cow: [Comprar]", delta_color="normal")
-            elif 'sell' in fundamentos['recommendationKey'].values[0]:
-                st.metric("Recomendação", "Vender", delta_color="inverse")
-                #st.metric("Recomendação", ":bear: [Vender]", delta_color="inverse")
-        if fundamentos['numberOfAnalystOpinions'].values[0] is not None:
-            st.metric('Nº de Opiniões de Analistas', fundamentos['numberOfAnalystOpinions'].values[0])
-        if fundamentos['targetMedianPrice'].values[0] is not None:
-            st.metric('Mediana de Preço alvo', f"R$ {fundamentos['targetMedianPrice'].values[0]:.2f}")
-
-    with col3:
         if fundamentos['targetHighPrice'].values[0] is not None:
             st.metric('Preço alvo máximo', f"R$ {fundamentos['targetHighPrice'].values[0]:.2f}")
+        
         if fundamentos['targetLowPrice'].values[0] is not None:
             st.metric('Preço alvo mínimo', f"R$ {fundamentos['targetLowPrice'].values[0]:.2f}")
-        if fundamentos['targetMeanPrice'].values[0] is not None:
-            st.metric('Média de Preço alvo', f"R$ {fundamentos['targetMeanPrice'].values[0]:.2f}")
+        
 
     mostrar_fundamentos = st.checkbox('Mostrar todos fundamentos')
     if mostrar_fundamentos:
