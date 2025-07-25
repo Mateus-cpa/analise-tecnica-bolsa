5949669c44a02ccebc8e5db9bf29181eb695276b
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-07-25 08:12:12 -0300

    :sparkles: feat: inclui alguns gráficos iniciais

--- src/analise_setorial.py
+++ src/analise_setorial.py
@@ -1,6 +1,12 @@
-
 import pandas as pd
-
+import streamlit as st
+import plotly.express as px
 
 def analise_setorial(df):
-    pass
+    # Gráfico de contagem de tickers por grupo
+    grafico_qtd = px.histogram(df, x="grupo", title="Contagem de Tickers por Grupo")
+    st.plotly_chart(grafico_qtd)
+    grafico_variacao = px.histogram(df, x="variacao_valor", title="Distribuição da Variação Percentual")
+    st.plotly_chart(grafico_variacao)
+    grafico_rendimento = px.histogram(df, x="rendimento", title="Distribuição do Retorno")
+    st.plotly_chart(grafico_rendimento)
