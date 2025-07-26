9bddd61925a16cabea4907d9b9ddb7940f32cba8
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-07-26 09:19:44 -0300

    :sparkles: feat: gráficos em andamento

--- src/analise_setorial.py
+++ src/analise_setorial.py
@@ -4,9 +4,31 @@ import plotly.express as px
 
 def analise_setorial(df):
     # Gráfico de contagem de tickers por grupo
-    grafico_qtd = px.histogram(df, x="grupo", title="Contagem de Tickers por Grupo")
+    grafico_qtd = px.histogram(df, 
+                               x="grupo", 
+                               title="Contagem de Tickers por Grupo",
+                               color="grupo")
     st.plotly_chart(grafico_qtd)
-    grafico_variacao = px.histogram(df, x="variacao_valor", title="Distribuição da Variação Percentual")
+
+    # Gráfico de variação percentual
+    grafico_variacao = px.histogram(df[(df['variacao_valor']>-20) & (df['variacao_valor']<20)], 
+                                     x="variacao_valor", 
+                                     title="Distribuição da Variação Percentual",
+                                     color="grupo")
     st.plotly_chart(grafico_variacao)
-    grafico_rendimento = px.histogram(df, x="rendimento", title="Distribuição do Retorno")
+
+    # Gráfico de rendimento
+    grafico_rendimento = px.histogram(df[df['rendimento'] <= 30], 
+                                       x="rendimento", 
+                                       title="Distribuição do Retorno",
+                                       color="grupo")
     st.plotly_chart(grafico_rendimento)
+    
+    # Gráfico de PVP
+    grafico_pvp = px.histogram(df[df['pvp'] <= 10], 
+                               x="pvp", 
+                               title="Distribuição do P/VPA",
+                               color="grupo")
+    st.plotly_chart(grafico_pvp)
+    # utilizar ytdata-profiling para análise mais detalhada
+    
