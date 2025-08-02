52b9a7c129020b16ce6e0a01f3d5267f6d6ebb0c
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-08-02 10:23:41 -0300

    :sparkles: feat: ajusta gráficos por valor de mercado

--- src/analise_setorial.py
+++ src/analise_setorial.py
@@ -23,46 +23,72 @@ def analise_setorial():
                             key="nome_coluna_setorial")
     coluna = "setor_pt" if nome_coluna == "Setor" else "industria_pt"
     
-    # Gráfico de contagem de tickers por grupo
     col1, col2 = st.columns([0.4, 0.6])
-    grafico_qtd = px.histogram(df, 
-                               x= 'grupo',
-                               title="Contagem de Tickers por Grupo",
-                               color="grupo",
-                               #mortar ao apontar no gráfico os tickers
-                               hover_data=["ticker", "setor_pt", "industria_pt"]
-                               )
+    # Gráficos de contagem ou soma de valor de mercado por grupo
+    if dimensao_graficos == "Valor de Mercado":
+        grafico_qtd = px.histogram(df, 
+                                   x='grupo',
+                                   y='valor_mercado',
+                                   title="Soma do Valor de Mercado por Grupo",
+                                   color="grupo",
+                                   histfunc="sum",
+                                   hover_data=["ticker", "setor_pt", "industria_pt"])
+        grafico_qtd.update_yaxes(title_text="Valor de Mercado (R$)")
+    else:
+        grafico_qtd = px.histogram(df, 
+                                   x='grupo',
+                                   title="Contagem de Tickers por Grupo",
+                                   color="grupo",
+                                   hover_data=["ticker", "setor_pt", "industria_pt"])
+        grafico_qtd.update_yaxes(title_text="Contagem")
     grafico_qtd.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
     grafico_qtd.update_xaxes(title_text="Grupo")
-    grafico_qtd.update_yaxes(title_text="Contagem")
     grafico_qtd.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
-    grafico_qtd.update_layout(bargap=0.2)  # Ajusta o espaçamento entre as barras
+    grafico_qtd.update_layout(bargap=0.2)
     col1.plotly_chart(grafico_qtd)
 
-    # Gráfico de contagem de tickers por setor/indústria
-    grafico_qtd_por_setor = px.histogram(df, 
-                                         y=coluna,
-                                         title= f"Contagem de Tickers por {nome_coluna}",
-                                         color="grupo"
-                                         )
+    # Gráficos de contagem de tickers por setor/indústria
+    if dimensao_graficos == "Valor de Mercado":
+        grafico_qtd_por_setor = px.histogram(df, 
+                                             y=coluna,
+                                             x='valor_mercado',
+                                             title=f"Soma do Valor de Mercado por {nome_coluna}",
+                                             color="grupo",
+                                             histfunc="sum")
+        grafico_qtd_por_setor.update_xaxes(title_text="Valor de Mercado (R$)")
+    else:
+        grafico_qtd_por_setor = px.histogram(df, 
+                                             y=coluna,
+                                             title=f"Contagem de Tickers por {nome_coluna}",
+                                             color="grupo")
+        grafico_qtd_por_setor.update_xaxes(title_text="Contagem")
     grafico_qtd_por_setor.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.7))
     grafico_qtd_por_setor.update_yaxes(title_text=nome_coluna)
-    grafico_qtd_por_setor.update_xaxes(title_text="Contagem")
     grafico_qtd_por_setor.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
     grafico_qtd_por_setor.update_layout(bargap=0.2)
     col2.plotly_chart(grafico_qtd_por_setor)
     
-    # Gráfico de variação percentual com leganda abaixo do gráfico
-    grafico_variacao = px.histogram(df[(df['variacao_valor']>-20) & (df['variacao_valor']<20)], 
-                                     x="variacao_valor", 
-                                     title="Distribuição da Variação Percentual",
-                                     color="grupo",
-                                     labels={"variacao_valor": "Variação Percentual"})
+    # Gráficos de variação percentual
+    if dimensao_graficos == "Valor de Mercado":
+        grafico_variacao = px.histogram(df[(df['variacao_valor']>-20) & (df['variacao_valor']<20)], 
+                                         x="variacao_valor", 
+                                         y="valor_mercado",
+                                         title="Soma do Valor de Mercado por Variação Percentual",
+                                         color="grupo",
+                                         labels={"variacao_valor": "Variação Percentual"},
+                                         histfunc="sum")
+        grafico_variacao.update_yaxes(title_text="Valor de Mercado (R$)")
+    else:
+        grafico_variacao = px.histogram(df[(df['variacao_valor']>-20) & (df['variacao_valor']<20)], 
+                                         x="variacao_valor", 
+                                         title="Distribuição da Variação Percentual",
+                                         color="grupo",
+                                         labels={"variacao_valor": "Variação Percentual"})
+        grafico_variacao.update_yaxes(title_text="Contagem")
     grafico_variacao.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.7))
     grafico_variacao.update_xaxes(title_text="Variação Percentual")
-    grafico_variacao.update_yaxes(title_text="Contagem")
     grafico_variacao.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
-    grafico_variacao.update_layout(bargap=0.2)  # Ajusta
+    grafico_variacao.update_layout(bargap=0.2)
     col1.plotly_chart(grafico_variacao)
 
     boxplot_variacao = px.box((df[(df['variacao_valor']<50) & 
@@ -79,16 +105,25 @@ def analise_setorial():
     boxplot_variacao.update_layout(bargap=0.2)  # Ajusta
     col2.plotly_chart(boxplot_variacao)
 
-    # Gráfico de rendimento
-    grafico_rendimento = px.histogram(df[df['rendimento'] <= 30], 
-                                       x="rendimento", 
-                                       title="Distribuição do Retorno",
-                                       color="grupo")
+    # Gráficos de rendimento
+    if dimensao_graficos == "Valor de Mercado":
+        grafico_rendimento = px.histogram(df[df['rendimento'] <= 30], 
+                                           x="rendimento", 
+                                           y="valor_mercado",
+                                           title="Soma do Valor de Mercado por Retorno",
+                                           color="grupo",
+                                           histfunc="sum")
+        grafico_rendimento.update_yaxes(title_text="Valor de Mercado (R$)")
+    else:
+        grafico_rendimento = px.histogram(df[df['rendimento'] <= 30], 
+                                           x="rendimento", 
+                                           title="Distribuição do Retorno",
+                                           color="grupo")
+        grafico_rendimento.update_yaxes(title_text="Contagem")
     grafico_rendimento.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.75))
     grafico_rendimento.update_xaxes(title_text="Rendimento (%)")
-    grafico_rendimento.update_yaxes(title_text="Contagem")
     grafico_rendimento.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
-    grafico_rendimento.update_layout(bargap=0.2)  # Ajusta
+    grafico_rendimento.update_layout(bargap=0.2)
     col1.plotly_chart(grafico_rendimento)
 
     boxplot_rendimento = px.box(df[(df['rendimento']<30) & (df['rendimento']>0)],
@@ -103,16 +138,25 @@ def analise_setorial():
     boxplot_rendimento.update_layout(bargap=0.2)  # Ajusta
     col2.plotly_chart(boxplot_rendimento)
 
-    # Gráfico de PVP
-    grafico_pvp = px.histogram(df[df['pvp'] <= 10], 
-                               x="pvp", 
-                               title="Distribuição do P/VPA",
-                               color="grupo")
+    # Gráficos de PVP
+    if dimensao_graficos == "Valor de Mercado":
+        grafico_pvp = px.histogram(df[df['pvp'] <= 10], 
+                                   x="pvp", 
+                                   y="valor_mercado",
+                                   title="Soma do Valor de Mercado por P/VPA",
+                                   color="grupo",
+                                   histfunc="sum")
+        grafico_pvp.update_yaxes(title_text="Valor de Mercado (R$)")
+    else:
+        grafico_pvp = px.histogram(df[df['pvp'] <= 10], 
+                                   x="pvp", 
+                                   title="Distribuição do P/VPA",
+                                   color="grupo")
+        grafico_pvp.update_yaxes(title_text="Contagem")
     grafico_pvp.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
     grafico_pvp.update_xaxes(title_text="P/VPA")
-    grafico_pvp.update_yaxes(title_text="Contagem")
     grafico_pvp.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
-    grafico_pvp.update_layout(bargap=0.2)  # Ajusta
+    grafico_pvp.update_layout(bargap=0.2)
     col1.plotly_chart(grafico_pvp)
 
     boxplot_pvp = px.box(df[(df['pvp']<10) & (df['pvp']>0)],
