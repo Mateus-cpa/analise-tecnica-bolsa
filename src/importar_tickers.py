8d7721c95492699ab4eec003f71b43561acf1ed0
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-08-06 14:00:19 -0300

    :wrench: feat: várias camadas de try except para conexão com APIs

--- src/importar_tickers.py
+++ src/importar_tickers.py
@@ -43,69 +43,89 @@ def importar_tickers():
     print(f'Qtde. de FIIs:  {df_fiis.shape[0]}')
     df_fiis['grupo'] = 'FII'
     df_fiis['ticker'] = df_fiis['ticker'].apply(lambda x: str(x) + '11')
-    print(df_fiis)
+    print(df_fiis.head())
 
     # -- AÇÕES --
     print('Importando ações')
     url = "https://www.dadosdemercado.com.br/acoes"
-    request = requests.get(url)
-    content = request.content.decode('utf-8')
-    soup = bs4(content, 'html.parser')
-    table_container = soup.find('div', {'class': 'table-container'})
-    table = table_container.find('table') if table_container else None
-    if table is None:
-        raise ValueError("Tabela não encontrada no conteúdo HTML.")
-    df_acoes = pd.DataFrame(columns=['ticker', 'grupo'])
-    for row in table.find_all('tr')[1:]:
-        cols = row.find_all('td')
-        if len(cols) > 0:
-            df_acoes = df_acoes.append({'ticker': cols[0].text.strip(), 'grupo': 'Ação'}, ignore_index=True)
+    try:
+        request = requests.get(url)
+        content = request.content.decode('utf-8')
+        soup = bs4(content, 'html.parser')
+        table_container = soup.find('div', {'class': 'table-container'})
+        table = table_container.find('table') if table_container else None
+        if table is None:
+            raise ValueError("Tabela não encontrada no conteúdo HTML.")
+        df_acoes = pd.DataFrame(columns=['ticker', 'grupo'])
+        lista_acoes = []
+        for row in table.find_all('tr')[1:]:
+            cols = row.find_all('td')
+            if len(cols) > 0:
+                ticker = cols[0].text.strip()
+                lista_acoes.append(ticker)
+        df_acoes = pd.DataFrame(lista_acoes, columns=['ticker'])
+        df_acoes['grupo'] = 'Ação'
+    except Exception as e:
+        print(f"Erro ao buscar dados de ações: {e}")
+        df_acoes = pd.DataFrame(columns=['ticker', 'grupo'])
     print(f'Qtde. de ações:  {df_acoes.shape[0]}')
+    
 
     # -- ÍNDICES B3 --
     print('Importando índices')
-    url = "https://www.dadosdemercado.com.br/b3"
-    request = requests.get(url)
-    content = request.content.decode('utf-8')
-    soup = bs4(content, 'html.parser')
-    table_container = soup.find('div', {'class': 'table-container'})
-    table = table_container.find('table') if table_container else None
-    df_indices = pd.DataFrame(columns=['ticker','grupo'])
-    for row in table.find_all('tr')[1:]:
-        cols = row.find_all('td')
-        if len(cols) > 0:
-            df_indices = df_indices.append({'ticker': cols[0].text.strip(), 'grupo': 'Índice'}, ignore_index=True)
+    try:
+        url = "https://www.dadosdemercado.com.br/b3"
+        request = requests.get(url)
+        content = request.content.decode('utf-8')
+        soup = bs4(content, 'html.parser')
+        table_container = soup.find('div', {'class': 'table-container'})
+        table = table_container.find('table') if table_container else None
+        df_indices = pd.DataFrame(columns=['ticker','grupo'])
+        lista_indices = []
+        for row in table.find_all('tr')[1:]:
+            cols = row.find_all('td')
+            if len(cols) > 0:
+                ticker = cols[0].text.strip()
+                lista_indices.append(ticker)
+        df_indices = pd.DataFrame(lista_indices, columns=['ticker'])
+        df_indices['grupo'] = 'Índice'
+    except Exception as e:
+        print(f"Erro ao buscar dados de índices: {e}")
+        df_indices = pd.DataFrame(columns=['ticker', 'grupo'])
     print(f'Qtde. de índices:  {df_indices.shape[0]}')
 
     # -- Índices macroeconômicos --
-    importar_ipca_sidra()
+    #importar_ipca_sidra()
 
     # -- API BrAPI - stocks --
     print('Importando ações da API BrAPI')
     df_acoes_brapi = pd.DataFrame(columns=['ticker','grupo'])
     df_indices_brapi = pd.DataFrame(columns=['ticker','grupo'])
     url = "https://brapi.dev/api/available"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        df_acoes_brapi = data['stocks']
-        df_acoes_brapi['grupo'] ='Ações'
-        df_indices_brapi = data['indexes']
-        df_indices_brapi['grupo'] = 'Índice'
-        print(f"Tickers Ações brapi importados com sucesso: {df_acoes_brapi.shape[0]}")
-        print(f"Tickers Índices brapi importados com sucesso: {df_indices_brapi.shape[0]}")
-    else:
-        print(f"Erro ao acessar a API BrAPI: {response.status_code}")
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            df_acoes_brapi = pd.DataFrame(data['stocks'],columns='ticker')
+            df_acoes_brapi['grupo'] ='Ações'
+            df_indices_brapi = pd.DataFrame(data['indexes'],columns='ticker')
+            df_indices_brapi['grupo'] = 'Índice'
+            print(f"Tickers Ações brapi importados com sucesso: {df_acoes_brapi.shape[0]}")
+            print(f"Tickers Índices brapi importados com sucesso: {df_indices_brapi.shape[0]}")
+            #Classifica os tickers
+            df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'BDR' if x.ticker.endswith('34') or
+                                    x.ticker.endswith('39') else 'Ação', axis=1)
+            df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'ETF/Unit/FII' if x.ticker.endswith('11')
+                                else x.grupo, axis=1)
+            df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'Índice' if x.ticker.startswith('^') 
+                                else x.grupo, axis=1)
 
+        else:
+            print(f"Erro ao acessar a API BrAPI: {response.status_code}")
+    except Exception as e:
+        print(f'Erro ao buscar dados de BRAPI: {e}')
     
-    #Classifica os tickers
-    df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'BDR' if x.ticker.endswith('34') or
-                            x.ticker.endswith('39') else 'Ação', axis=1)
-    df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'ETF/Unit/FII' if x.ticker.endswith('11')
-                           else x.grupo, axis=1)
-    df_acoes_brapi['grupo'] = df_acoes_brapi.apply(lambda x: 'Índice' if x.ticker.startswith('^') 
-                           else x.grupo, axis=1)
-
+    
     # Anexa cada dataframe a df_total se não já tiver
     if 'df_total' not in locals():
         df_total = pd.DataFrame(columns=['ticker', 'grupo'])
@@ -122,14 +142,17 @@ def importar_tickers():
     url = f"https://brapi.dev/api/v2/crypto/available"
     df_criptos = pd.DataFrame(columns=['ticker', 'grupo'])
     response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        df_criptos = data['coins']
-        df_criptos['grupo'] = 'Criptomoeda'
-        print(f"Criptomoedas importadas da API BrAPI: {df_criptos.shape[0]}")
-    else:
-        print(f"Erro ao acessar a API BrAPI: {response.status_code}")
-
+    try:
+        if response.status_code == 200:
+            data = response.json()
+            df_criptos['ticker'] = data['coins']
+            df_criptos['grupo'] = 'Criptomoeda'
+            print(f"Criptomoedas importadas da API BrAPI: {df_criptos.shape[0]}")
+        else:
+            print(f"Erro ao acessar a API BrAPI: {response.status_code}")
+    except Exception as e:
+        print(f'Não foi possível importar dados de cripto: {e}')
+        
     df_total = pd.concat([df_total, df_criptos], ignore_index=True)
     
     # -- Mostra quantidade por grupo --
